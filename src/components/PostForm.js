56108774da04a202396b5875f39939ae1368a419
Timuçin Alpay Bohur <140290974+AlpayBohur@users.noreply.github.com>
2024-03-21 04:39:05 +0300

    son

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -3,13 +3,17 @@ import { useForm } from "react-hook-form";
 import { nanoid } from "nanoid";
 import { useHistory } from "react-router";
 import Gratitude from "./../assets/grForm.png";
+import { useDispatch } from "react-redux";
+import { notEkleAPI } from "../actions";
 
-export default function PostForm() {
+export default function PostForm({ toast_add }) {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({ mode: "onChange" });
+
+  const dispatch = useDispatch();
 
   const history = useHistory();
 
@@ -21,11 +25,9 @@ export default function PostForm() {
         .filter((v) => v !== "")
         .join("|"),
     };
-
-    // burada ilgili eylemi dispatch edin
-    // toast mesajı gösterin
-    // sonra aşağıdaki satırı aktifleştirin
-    // setTimeout(() => history.push("/notlar"), 2000);
+    dispatch(notEkleAPI(yeniNot));
+    toast_add();
+    setTimeout(() => history.push("/notlar"), 2000);
   }
 
   const inputCx = "border border-zinc-300 h-9 rounded-none text-sm px-2 w-full";
@@ -35,7 +37,6 @@ export default function PostForm() {
       <div className="flex-1">
         <img src={Gratitude} alt="" className="block object-cover h-full" />
       </div>
-
 
       <form
         onSubmit={handleSubmit(onSubmit)}
@@ -48,8 +49,8 @@ export default function PostForm() {
           yansıtmalara kadar pek çok şeyden oluşabilir.
         </p>
         <p className="text-stone-700 my-3 text-xs">
-          Her gün belli saatlerde 3 maddeden oluşan bir liste
-          yapmak, bu alışkanlığa iyi bir başlangıç noktası sayılır.
+          Her gün belli saatlerde 3 maddeden oluşan bir liste yapmak, bu
+          alışkanlığa iyi bir başlangıç noktası sayılır.
         </p>
         <div>
           <input
@@ -76,10 +77,7 @@ export default function PostForm() {
           />
         </div>
 
-        <button
-          type="submit"
-          className="myButton"
-        >
+        <button type="submit" className="myButton">
           Ekle
         </button>
       </form>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import myReducer from "./reducers";
+import thunk from "redux-thunk";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
-    <>
-      <App />
-    </>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <>
+        <App />
+      </>
+    </BrowserRouter>
+  </Provider>
 );
